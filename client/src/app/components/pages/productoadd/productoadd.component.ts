9db668b27aae9efc060cf06bef82280c1ad0296d
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-03 16:49:43 -0500

    Se crearon las funciones de guardar y actualizar

--- client/src/app/components/pages/productoadd/productoadd.component.ts
+++ client/src/app/components/pages/productoadd/productoadd.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductDataService } from '../../../services/product-data.service';
+import { ProductInterface } from '../../../models/producto.interface';
+import { NgForm } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-productoadd',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoaddComponent implements OnInit {
 
-  constructor() { }
+  public selectedProduct: ProductInterface = {
+    id: "",
+    nombre: "",
+    descripcion: "",
+    imagen: "",
+    precio: 0,
+    cantidad: 0,
+  };
 
-  ngOnInit(): void {
+  edit: boolean = false;
+
+  constructor(private dataApi: ProductDataService, private location: Location, private activedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    const params = this.activedRoute.snapshot.params;
+    if (params.id) {
+      this.dataApi.getProductById(params.id)
+        .subscribe(res => {
+          this.selectedProduct = res;
+          this.edit = true;
+        }, err => { console.log(err) });
+    }
+
+  }
+
+  onSaveProduct(productForm: NgForm) {
+
+    this.dataApi.createProduct(productForm.value).subscribe(producto => this.location.back())
+  }
+  onUpdateProduct(productForm: NgForm) {
+    this.dataApi.updateProduct(this.selectedProduct.id, this.selectedProduct).subscribe(producto => this.location.back())
   }
 
 }
