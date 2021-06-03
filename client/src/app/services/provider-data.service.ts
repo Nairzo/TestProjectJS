3a2f76d9d230063325faf743fa7af9af83460305
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-03 18:01:49 -0500

    Se agregaron las funciones CRUD

--- client/src/app/services/provider-data.service.ts
+++ client/src/app/services/provider-data.service.ts
@@ -2,15 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { map} from 'rxjs/operators';
-import { ProductInterface } from '../models/producto.interface';
+import { ProviderInterface } from '../models/proveedor.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProviderDataService {
 
+  proveedor!: Observable<any>;
+  proveedores!: Observable<any>;
   url_api = "http://localhost:3000/api";
-  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  headers: HttpHeaders = new HttpHeaders({
+    "Content-type": "application/json"
+  });
+
+  getAllProviders() {
+    return this.http.get(`${this.url_api}/productos`);
+  }
+
+  getProviderById(id: string) {
+    return this.http.get(`${this.url_api}/productos/${id}`);
+  }
+  createProvider(producto: ProviderInterface){
+    return this.http.post<ProviderInterface>(`${this.url_api}/productos/`, producto, {headers: this.headers}).pipe(map(data => data));
+  }
+  updateProvider(id: string|undefined, updatedGame: ProviderInterface): Observable<ProviderInterface>{
+    return this.http.put(`${this.url_api}/productos/`, updatedGame, {headers: this.headers}).pipe(map(data => data));
+
+  }
+  deleteProvider(id: string){
+    return this.http.delete<ProviderInterface>(`${this.url_api}/productos/${id}`, {headers: this.headers}).pipe(map(data => data));
+  }
+
 }
