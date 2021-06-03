7d98628adc3bd629ba94ef6593a84d9bfc9f7ffc
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-03 16:38:47 -0500

    Se agregaron funciones para actualizar y borrar

--- client/src/app/components/pages/producto/producto.component.ts
+++ client/src/app/components/pages/producto/producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductDataService } from '../../../services/product-data.service';
+import { ActivatedRoute, Params} from '@angular/router';
+import { ProductInterface } from '../../../models/producto.interface';
 
 @Component({
   selector: 'app-producto',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
+  
+  constructor(route: ActivatedRoute, private dataApi: ProductDataService ) {
+    const producto_id: string = route.snapshot.params["id"];
+    this.getDetails(producto_id);
+   }
+  private producto: ProductInterface = {
+    nombre: '',
+    descripcion: '',
+    imagen: '',
+    precio: 0,
+    cantidad: 0
+  }
 
-  constructor() { }
+  ngOnInit() {
+    
+  }
 
-  ngOnInit(): void {
+  getDetails(id: string){
+    this.dataApi.getProductById(id).subscribe(producto => (this.producto = producto))
+  }
+
+  onDelete(id: string){
+    if(confirm("¿Estas seguro de eliminar este producto?")){
+      this.dataApi.deleteProduct(id).subscribe();
+    }
+  }
+  onPreUpdateProduct(id: string) {
+    console.log(id);
   }
 
 }
