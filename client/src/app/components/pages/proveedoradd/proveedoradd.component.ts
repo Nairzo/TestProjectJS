d37a8e80a351c3db97da05e96f2274796ea4a730
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-03 23:56:13 -0500

    Se agregarón las funciones de borrar y actualizar

--- client/src/app/components/pages/proveedoradd/proveedoradd.component.ts
+++ client/src/app/components/pages/proveedoradd/proveedoradd.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderDataService } from '../../../services/provider-data.service';
+import { ProviderInterface } from '../../../models/proveedor.interface';
+import { NgForm } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-proveedoradd',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProveedoraddComponent implements OnInit {
 
-  constructor() { }
+  public selectedProvider: ProviderInterface = {
+    nombre: "",
+    estado: "",
+    telefono: 0,
+    rfc: "",
+    imagen: "",
+    id: ""
+  };
 
-  ngOnInit(): void {
+  edit: boolean = false;
+
+  constructor(private dataApi: ProviderDataService, private location: Location, private activedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    const params = this.activedRoute.snapshot.params;
+    if (params.id) {
+      this.dataApi.getProviderById(params.id)
+        .subscribe(res => {
+          this.selectedProvider = res;
+          this.edit = true;
+        }, err => { console.log(err) });
+    }
+  }
+
+  onSaveProvider(providerForm: NgForm) {
+    this.dataApi.createProvider(providerForm.value).subscribe(proveedor => this.location.back())
+  }
+  onUpdateProvider(providerForm: NgForm) {
+    this.dataApi.updateProvider(this.selectedProvider.id, this.selectedProvider).subscribe(proveedor => this.location.back())
   }
 
 }
