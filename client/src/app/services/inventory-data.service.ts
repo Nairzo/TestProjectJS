bd29a32fd82e382cea5e03b244dbdb08eff7cb0d
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-04 16:21:35 -0500

    Se creó la función para obtener la lista

--- client/src/app/services/inventory-data.service.ts
+++ client/src/app/services/inventory-data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryDataService {
 
-  constructor() { }
+  url_api = "http://localhost:3000/api";
+
+  constructor(private http: HttpClient) { }
+
+  getAllInventory() {
+    return this.http.get(`${this.url_api}/inventarios`);
+  }
 }
