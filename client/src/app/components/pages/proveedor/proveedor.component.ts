9a72ef8c786f7fa07965b7517e90e6678eee6e6e
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-03 22:49:50 -0500

    Se agregaron la funciones de eliminar y actualizar

--- client/src/app/components/pages/proveedor/proveedor.component.ts
+++ client/src/app/components/pages/proveedor/proveedor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderDataService } from '../../../services/provider-data.service';
+import { ActivatedRoute, Params} from '@angular/router';
+import { ProviderInterface } from '../../../models/proveedor.interface';
 
 @Component({
   selector: 'app-proveedor',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProveedorComponent implements OnInit {
 
-  constructor() { }
+  constructor(route: ActivatedRoute, private dataApi: ProviderDataService) { 
+    const proveedor_id: string = route.snapshot.params["id"];
+    this.getDetails(proveedor_id);
+  }
+  private proveedor: ProviderInterface = {
+    nombre: '',
+    estado: '',
+    rfc: '',
+    imagen: '',
+    telefono: 0
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  getDetails(id: string){
+    this.dataApi.getProviderById(id).subscribe(proveedor => (this.proveedor = proveedor))
+  }
+
+  onDelete(id: string){
+    if(confirm("¿Estas seguro de eliminar este proveedor?")){
+      this.dataApi.deleteProvider(id).subscribe();
+    }
   }
 
 }
