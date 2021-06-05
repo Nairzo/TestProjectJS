b8fba7716b9899497a0051d288346a45af72b31b
adrian <rodolfoadrianmunoz@gmail.com>
2021-06-05 06:23:21 -0500

    Se creó la función para añadir inventario

--- client/src/app/services/inventory-data.service.ts
+++ client/src/app/services/inventory-data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { map} from 'rxjs/operators';
+import { InventoryInterface } from '../models/inventario.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +12,14 @@ export class InventoryDataService {
   url_api = "http://localhost:3000/api";
 
   constructor(private http: HttpClient) { }
+  headers: HttpHeaders = new HttpHeaders({
+    "Content-type": "application/json"
+  });
 
   getAllInventory() {
     return this.http.get(`${this.url_api}/inventarios`);
+  }
+  createInventory(inventario: InventoryInterface){
+    return this.http.post<InventoryInterface>(`${this.url_api}/inventarios/`, inventario, {headers: this.headers}).pipe(map(data => data));
   }
 }
